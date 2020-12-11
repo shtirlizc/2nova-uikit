c5dcf83adb647f16ee0c16ab74ff8910dbc47731
Marat Sarvarov <shtirlizc@gmail.com>
2020-12-11 12:23:15 +0500

    cra working

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
     stories: ['../src/**/*.stories.tsx'],
-    addons: ['@storybook/addon-docs'],
+    addons: ['@storybook/addon-docs', '@storybook/preset-create-react-app'],
 };
