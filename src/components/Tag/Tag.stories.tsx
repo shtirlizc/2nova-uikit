cfea4911dac0ed7695abd9b5b4595dcd4bda9f0a
Marat Sarvarov <shtirlizc@gmail.com>
2020-12-11 12:16:56 +0500

    add empty storybook

---
+++ src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tag from './index';
+
+export default {
+    title: 'Tag title',
+    component: Tag
+}
+
+export const Basic = () => (
+    <Tag />
+)
+
+export const Second = () => (
+    <Tag title="My second tag" />
+)
