e797b496f4f5d3261f410a8fa3a5330637fafb14
Marat Sarvarov <shtirlizc@gmail.com>
2020-12-11 16:56:58 +0500

    add one font to storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = async ({ config }) => {
+  // fonts
+  config.module.rules.push({
+    test: /\.(png|eot|otf|ttf|woff|woff2|svg)$/,
+    use: [
+      {
+        loader: "file-loader",
+        query: {
+          name: "[name].[ext]",
+        },
+      },
+    ],
+    include: path.resolve(__dirname, "../"),
+  });
+
+  return config;
+};
