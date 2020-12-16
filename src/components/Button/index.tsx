b95e0d05f2896ab524faa98e07b57b9790c357c3
Andrey Elpaev <andrey.elpaev@2nova.ru>
2020-12-17 01:55:29 +0500

    fix: add valid props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ import s from "./Button.module.css";
 
 export type ButtonProps = {
   type?: "primary" | "secondary" | "outlined";
-  size?: "s" | "m" | "l" | "xl",
+  size?: "sm" | "md" | "lg" | "xl",
 };
 
 const Button: React.FC<ButtonProps> = ({
