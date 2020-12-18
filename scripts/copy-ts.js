bc892b52ec62cd6886f978ed4d0ac461f7f0aa45
Andrey Elpaev <andrey.elpaev@2nova.ru>
2020-12-18 18:40:13 +0500

    fix: copy-ts (temporary script)

---
+++ scripts/copy-ts.js
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const glob = require('glob');
+const fse = require('fs-extra');
+/* eslint-enable @typescript-eslint/no-var-requires */
+
+const srcDir = path.join('./src');
+const distDir = path.join('./dist/js');
+
+const files = glob.sync('**/*.d.ts', {
+    cwd: srcDir
+});
+
+files.forEach(file => {
+    const from = path.join(srcDir, file);
+    const to = path.join(distDir, file);
+    fse.copySync(from, to);
+});
+
+const outDir = path.join('dist');
+
+const copyPackageJson = async (distPath) => {
+    const pack = await fse.readJSON('package.json');
+
+    delete pack.devDependencies;
+    delete pack.scripts;
+    delete pack.browserslist;
+    delete pack.release;
+    delete pack.plugins;
+
+    const out  = `${distPath}/package.json`;
+    await fse.writeJSON(out, pack, { space: 2 });
+}
+
+(async () => {
+    await copyPackageJson(outDir);
+})();
