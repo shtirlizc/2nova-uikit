f7907caae412ce568c14f7d21d697d92aa34434c
Andrey Elpaev <andrey.elpaev@2nova.ru>
2020-12-18 19:14:53 +0500

    fix: add prop xl

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ import s from "./Button.module.css";
 
 export type ButtonProps = {
   type?: "primary" | "secondary" | "outlined";
-  size?: "sm" | "md" | "lg";
+  size?: "sm" | "md" | "lg" | "xl";
 };
 
 const Button: React.FC<ButtonProps> = ({
