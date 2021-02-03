52b8f4fdd163ccc4bcdeef8ea6aec3fbcd1a5539
Marat Sarvarov <shtirlizc@gmail.com>
2021-02-03 17:18:21 +0500

    fix: свойство onClick для конпок

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,14 +6,18 @@ import s from "./Button.module.css";
 export type ButtonProps = {
   type?: "primary" | "secondary" | "outlined";
   size?: "sm" | "md" | "lg" | "xl";
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 export const Button: React.FC<ButtonProps> = ({
   type = "primary",
   size = "sm",
+  onClick,
   children,
 }) => {
   return (
-    <button className={classNames(s.root, s[type], s[size])}>{children}</button>
+    <button className={classNames(s.root, s[type], s[size])} onClick={onClick}>
+      {children}
+    </button>
   );
 };
