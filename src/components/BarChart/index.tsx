44842c271974091955fe00ae7897af284553cdac
Andrey Elpaev <andrey.elpaev@2nova.ru>
2021-03-03 12:40:18 +0500

    fix: BarChart autoFit option

--- src/components/BarChart/index.tsx
+++ src/components/BarChart/index.tsx
@@ -1,88 +1,91 @@
 import React, { useEffect } from "react";
-import { Chart } from '@antv/g2';
-
+import { Chart } from "@antv/g2";
 
 export type BarChartProps = {
-    height?: number;
-    data: any[];
-    container?: string;
-    colors?: Array<string>
+  height?: number;
+  data: any[];
+  container?: string;
+  colors?: Array<string>;
 };
 
-export const BarChart: React.FC<BarChartProps> = ({ data = [], height = 300, container, colors = ['#FFDD00', '#D3D9DE'] }) => {
-    let chart: Chart;
+export const BarChart: React.FC<BarChartProps> = ({
+  data = [],
+  height = 300,
+  container,
+  colors = ["#FFDD00", "#D3D9DE"],
+}) => {
+  let chart: Chart;
 
-    useEffect(() => {
-        if (!chart) return;
-        chart.changeData(data);
-    }, [data])
+  useEffect(() => {
+    if (!chart) return;
+    chart.changeData(data);
+  }, [data]);
 
+  useEffect(() => {
+    if (chart) {
+      chart.destroy();
+    }
 
-    useEffect(() => {
+    chart = new Chart({
+      container: container || "chart",
+      height,
+      autoFit: true,
+      width: 615,
+    });
+    chart.scale("value", {
+      alias: "Количество",
+      nice: true,
+    });
+    chart.data(data);
+    chart.axis("value", {
+      title: {
+        offset: 30,
+      },
+      tickLine: {
+        style: {
+          stroke: "#D3D9DE",
+        },
+      },
+      line: {
+        style: {
+          stroke: "#D3D9DE",
+        },
+      },
+      grid: {
+        line: {
+          type: "line",
+          style: {
+            lineDash: [2],
+          },
+        },
+      },
+    });
 
-        if (chart) {
-            chart.destroy();
-        }
+    chart
+      .interval()
+      .position("name*value")
+      .color("type", colors)
+      .adjust("stack")
 
-        chart = new Chart({
-            container: container || "chart",
-            height,
-            width: 615
-        });
-        chart.scale('value', {
-            alias: 'Количество',
-            nice: true,
-        });
-        chart.data(data);
-        chart.axis('value', {
-            title: {
-                offset: 30,
-            },
-            tickLine: {
-                style: {
-                    stroke: '#D3D9DE'
-                }
-            },
-            line: {
-                style: {
-                    stroke: '#D3D9DE'
-                }
-            },
-            grid: {
-                line: {
-                    type: 'line',
-                    style: {
-                        lineDash: [2]
-                    }
-                }
-            }
-        });
+      .label("value", (val) => {
+        return {
+          position: "middle",
+          offset: 0,
+          content: (originData) => {
+            return originData.value;
+          },
+          style: {
+            fill: "#5E7080",
+          },
+        };
+      });
 
-        chart
-            .interval()
-            .position('name*value')
-            .color('type', colors)
-            .adjust('stack')
-            .label('value', (val) => {
-                return {
-                    position: 'middle',
-                    offset: 0,
-                    content: (originData) => {
-                        return originData.value;
-                    },
-                    style: {
-                        fill: '#5E7080'
-                    }
-                };
-            });
+    chart.legend({
+      position: "bottom",
+    });
 
-        chart.legend({
-            position: 'bottom',
-        });
+    chart.render();
+  }, []);
 
-        chart.render();
-    }, []);
-
-
-    return <div id="chart" />
+  return <div id="chart" />;
 };
