917673e1573c2d1ec4928b2d8fa56e9351842c1c
Marat Sarvarov <shtirlizc@gmail.com>
2021-03-13 07:52:06 +0500

    fix: Добавил для таблиц внутренную прокрутку и фон

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -15,49 +15,49 @@ type TableProps = {
 };
 
 export const Table: React.FC<TableProps> = ({ columns, rows }) => {
-  console.log("###: rows", rows);
-
   return (
-    <table className={s.table}>
-      <thead className={s.thead}>
-        <tr>
-          {columns.map(({ id, headerName }) => (
-            <th key={id} className={s.th}>
-              {headerName}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row) => {
-          const [key] = Object.values(row);
+    <div className={s.root}>
+      <table className={s.table}>
+        <thead className={s.thead}>
+          <tr>
+            {columns.map(({ id, headerName }) => (
+              <th key={id} className={s.th}>
+                {headerName}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row) => {
+            const [key] = Object.values(row);
 
-          return (
-            <tr key={key} className={s.tr}>
-              {Object.entries(row).map((td, idx) => {
-                const [field, value] = td;
-                const [currentColumn] = columns.filter(
-                  (col) => col.field === field
-                );
+            return (
+              <tr key={key} className={s.tr}>
+                {Object.entries(row).map((td, idx) => {
+                  const [field, value] = td;
+                  const [currentColumn] = columns.filter(
+                    (col) => col.field === field
+                  );
 
-                if (field === "id") {
-                  return null;
-                }
+                  if (field === "id") {
+                    return null;
+                  }
 
-                return (
-                  <td
-                    key={field}
-                    className={classNames(s.td, idx === 1 && s.tdRowName)}
-                  >
-                    <span>{currentColumn.headerName}</span>
-                    {value}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+                  return (
+                    <td
+                      key={field}
+                      className={classNames(s.td, idx === 1 && s.tdRowName)}
+                    >
+                      <span>{currentColumn.headerName}</span>
+                      {value}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
