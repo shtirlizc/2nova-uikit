12133c219d13fee280eb82cd656b81d9b1e2e921
Marat Sarvarov <shtirlizc@gmail.com>
2021-03-14 00:21:39 +0500

    fix: Экспорт табов

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,4 +2,5 @@ export * from "./Button";
 export * from "./PieChart";
 export * from "./Select";
 export * from "./Table";
-export * from './BarChart';
+export * from "./BarChart";
+export * from "./Tabs";
